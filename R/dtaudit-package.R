e484b4c30e20ead8f4fc17652095635aa5e92640
fpcordeiro <fernandolpcordeiro@gmail.com>
2026-02-07 07:58:05 -0500

    Remove regression-tables.R and excel-export.R
    These modules (clean_etable, export_coef_table, save_to_excel) are
    unrelated to data auditing and dilute the package scope. Also clean up
    dead imports (stri_extract_*, stri_replace_all_fixed, stri_sub,
    modifyList) and dead globalVariables (part, statistic, term) from
    dtaudit-package.R.

--- R/dtaudit-package.R
+++ R/dtaudit-package.R
@@ -2,18 +2,16 @@
 "_PACKAGE"
 
 #' @import data.table
-#' @importFrom stringi stri_detect_regex stri_extract_first_regex
-#'   stri_extract_last_regex stri_paste stri_replace_all_fixed
-#'   stri_replace_all_regex stri_replace_last_regex stri_split_fixed
-#'   stri_sub stri_trans_general stri_trans_nfkc stri_trans_tolower
+#' @importFrom stringi stri_detect_regex stri_paste stri_replace_all_regex
+#'   stri_split_fixed stri_trans_general stri_trans_nfkc stri_trans_tolower
 #'   stri_trans_toupper stri_trim_both
 #' @importFrom stats quantile sd
-#' @importFrom utils head modifyList
+#' @importFrom utils head
 NULL
 
 # Suppress R CMD check notes for data.table NSE variables
 utils::globalVariables(c(
   ".", ".N", ".SD", "..by.x", "..key_cols", "..num_cols", "..keys", "..vars",
-  "examples", "lower", "n_variants", "N", "N.x", "N.y", "ORIG_CJ__", "ORIG_DT__",
-  "original", "part", "row_names", "statistic", "term", "variable"
+  "examples", "lower", "n_na", "n_variants", "N", "N.x", "N.y", "ORIG_CJ__",
+  "ORIG_DT__", "original", "row_names", "variable"
 ))
